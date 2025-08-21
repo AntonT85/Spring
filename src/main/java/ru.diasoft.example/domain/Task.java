ae9a6ab2ff4142cdf77c85043c57507a1e4d9fed
atuzov <atuzov@diasoft.ru>
2025-08-21 19:48:45 +0300

    task4

--- src/main/java/ru.diasoft.example/domain/Task.java
+++ src/main/java/ru.diasoft.example/domain/Task.java
@@ -1,15 +1,29 @@
 package ru.diasoft.example.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "task")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "name")
     private String name;
 }
--- src/main/java/ru.diasoft.example/service/TaskServiceImpl.java
+++ src/main/java/ru.diasoft.example/service/TaskServiceImpl.java
@@ -1,50 +1,51 @@
 package ru.diasoft.example.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Optional;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.diasoft.example.dao.TaskRepository;
 import ru.diasoft.example.domain.Task;
 import ru.diasoft.example.exception.NoSuchTaskException;
 
 @Service
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
-    private final List<Task> taskList;
-    private final AtomicLong atomicLong;
-
-    public TaskServiceImpl() {
-        this.taskList = new ArrayList<>();
-        this.atomicLong = new AtomicLong();
-    }
+    private final TaskRepository repository;
 
     @Override
     public Task create(Task task) {
-        task.setId(atomicLong.incrementAndGet());
-        taskList.add(task);
-        return task;
+        return repository.save(task);
     }
     @Override
     public Task update(long id, Task task) {
-        taskList.removeIf(p -> p.getId() == id);
-        task.setId(id);
-        taskList.add(task);
-        return task;
+        Task updateTask = repository.findById(id)
+                .orElseThrow(() -> new NoSuchTaskException("Task with id = " + id + " not found"));
+
+        updateTask.setName(task.getName());
+
+        return repository.save(updateTask);
     }
     @Override
     public List<Task> getAll() {
-        return taskList;
+        return repository.findAll();
     }
     @Override
     public Task getById(long id) {
-        return taskList.stream()
-                .filter(item -> item.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new NoSuchTaskException("Task with id = " + id + " not found"));
+        Optional<Task> task = repository.findById(id);
+        if (task.isEmpty()) {
+            throw new NoSuchTaskException("Task with id = " + id + " not found");
+        }
+        return task.get();
     }
     @Override
     public void deleteById(long id) {
-        taskList.removeIf(p -> p.getId() == id);
+        Task task = repository.findById(id)
+                .orElseThrow(() -> new NoSuchTaskException("Task with id = " + id + " not found"));
+
+        repository.delete(task);
+
     }
 }
